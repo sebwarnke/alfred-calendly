bfc9f0dd17e457d6d14c60068a746d7c5522ce5f
Sebastian Warnke <sw-github@sebwarnke.com>
2021-02-27 17:37:55 +0100

    rename helper.py and change workflow name

---
+++ src/helper.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+import constants as c
+
+def reset_workflow_config(wf):
+    try:
+        wf.clear_settings()
+        wf.clear_cache()
+        wf.delete_password(c.CLIENT_ID)
+        wf.delete_password(c.CLIENT_SECRET)
+        wf.delete_password(c.REFRESH_TOKEN)
+        wf.delete_password(c.ACCESS_TOKEN)
+    except:
+        pass
