611affc1d22b3401a86e5334d2bd79cab2a2b59e
Sebastian Warnke <sw-github@sebwarnke.com>
2021-03-15 23:25:01 +0100

    add client methods

--- src/calendly_client.py
+++ src/calendly_client.py
@@ -7,6 +7,10 @@ from workflow import Workflow3, web
 log = Workflow3().logger
 
 
+def active_filter(event_type):
+    return event_type.get("active")
+
+
 class CalendlyClient:
 
     client_id = ""
@@ -87,25 +91,52 @@ class CalendlyClient:
     def create_scheduling_link(self):
         pass
 
-    def get_event_types_of_user(self, user, access_token):
+    def get_event_types_of_user(self, user, access_token, page_token=None):
         log.debug("in: get_event_types_of_user")
+
+        request_params = {
+            "user": user
+        }
+        if page_token is not None:
+            request_params["page_token"] = page_token
+
         response = web.get(
             url="%s%s" % (c.CALENDLY_API_BASE_URL, c.CALENDLY_EVENT_TYPES_URI),
             headers={
                 "Authorization": "Bearer %s" % access_token,
             },
-            params={
-                "user": user
-            }
+            params=request_params
         )
         response.raise_for_status()
 
         if response.status_code == 200:
-            return response.json()["collection"]
+            return response.json()
         else:
             log.error("Getting Event Types failed. Calendly returned status [%s]. Response payload below." % response.status_code)
             log.error(response.json())
             return None
+
+    def get_all_event_types_of_user(self, user, access_token, the_filter=None):
+        log.debug("in: get_all_event_types_of_user")
+
+        next_page_uri = None
+
+        event_types = []
+
+        while True:
+            r = self.get_event_types_of_user(user, access_token, next_page_uri)
+            next_page_uri = r["pagination"]["next_page"]
+
+            event_types.extend(r.get("collection"))
+
+            if next_page_uri is None:
+                break
+
+        if the_filter is not None:
+            event_types = filter(the_filter, event_types)
+
+        return event_types
+
 
     def get_current_user(self, access_token):
         log.debug("in: get_current_user")
