47c52f95219f3953d49de5f5e0aa31e0080b336c
Sebastian Warnke <sw-github@sebwarnke.com>
2021-06-06 00:12:29 +0200

    fix tests - remove tests for legacy oauth api - fix constructor argument list

--- src/calendly_client_test.py
+++ src/calendly_client_test.py
@@ -4,21 +4,6 @@ from mock import patch
 from calendly_client import CalendlyClient
 from calendly_client import active_filter as ACTIVE_FILTER
 
-import constants as c
-
-RESPONSE_INTROSPECT_TRUE = {
-    "active": True
-}
-
-RESPONSE_INTROSPECT_FALSE = {
-    "active": False
-}
-
-RESPONSE_AUTHORIZE = {
-    "refresh_token": "b77a76ffce83d3bc20531ddfa76704e584f0ee963f6041b8bfc70c91373267d5",
-    "access_token": "ab32b480a9a755bd26421bc7e24e65dc055b039a3cf58430a5c5814a63c01d5a"
-}
-
 RESPONSE_GET_EVENT_TYPES_1 = {
     "collection": [
         {
@@ -72,47 +57,7 @@ class CalendlyClientTest(unittest.TestCase):
     calendly_client = None
 
     def setUp(self):
-        self.calendly_client = CalendlyClient("foo", "bar", "baz")
-
-    @patch("workflow.web.post")
-    def test_when_authorize_returns_200_then_dict_with_tokens_is_returned(self, mock_web_post):
-        response_mock = Mock()
-        response_mock.status_code = 200
-        response_mock.json.return_value = RESPONSE_AUTHORIZE
-
-        mock_web_post.return_value = response_mock
-
-        r = self.calendly_client.authorize("code")
-
-        assert r.get(c.ACCESS_TOKEN) == "ab32b480a9a755bd26421bc7e24e65dc055b039a3cf58430a5c5814a63c01d5a"
-        assert r.get(c.REFRESH_TOKEN) == "b77a76ffce83d3bc20531ddfa76704e584f0ee963f6041b8bfc70c91373267d5"
-
-    @patch("workflow.web.post")
-    def test_when_authorize_returns_401_then_dict_with_tokens_is_returned(self, mock_web_post):
-        response_mock = Mock()
-        response_mock.status_code = 401
-
-        mock_web_post.return_value = response_mock
-
-        calendly_client = CalendlyClient("foo", "bar", "baz")
-        r = calendly_client.authorize("code")
-
-        assert r is None
-
-    @patch("workflow.web.post")
-    def test_when_introspect_returns_200_then_value_of_active_is_returned(self, mock_web_post):
-        response_mock = Mock()
-        response_mock.status_code = 200
-
-        response_mock.json.return_value = RESPONSE_INTROSPECT_TRUE
-        mock_web_post.return_value = response_mock
-        r_true = self.calendly_client.introspect("access_token")
-        assert r_true is True
-
-        response_mock.json.return_value = RESPONSE_INTROSPECT_FALSE
-        mock_web_post.return_value = response_mock
-        r_false = self.calendly_client.introspect("access_token")
-        assert r_false is False
+        self.calendly_client = CalendlyClient("access_token")
 
     @patch("workflow.web.get")
     def test_when_get_event_types_returns_200_then_event_types_are_returned(self, mock_web_get):
@@ -122,7 +67,7 @@ class CalendlyClientTest(unittest.TestCase):
         response_mock.json.return_value = RESPONSE_GET_EVENT_TYPES_1
         mock_web_get.return_value = response_mock
 
-        r = self.calendly_client.get_event_types_of_user("user", "access_tocken")
+        r = self.calendly_client.get_event_types_of_user("user")
 
         expected = {
             "collection": [
@@ -154,7 +99,7 @@ class CalendlyClientTest(unittest.TestCase):
     def test_when_get_all_event_types_then_all_event_types_are_returned(self, get_event_types_of_user):
         get_event_types_of_user.side_effect = [RESPONSE_GET_EVENT_TYPES_1, RESPONSE_GET_EVENT_TYPES_2]
 
-        r = self.calendly_client.get_all_event_types_of_user("user", "access_token")
+        r = self.calendly_client.get_all_event_types_of_user("user")
 
         expected = [
             {
@@ -195,7 +140,7 @@ class CalendlyClientTest(unittest.TestCase):
     def test_when_get_all_active_event_types_then_active_event_types_are_returned(self, get_event_types_of_user):
         get_event_types_of_user.side_effect = [RESPONSE_GET_EVENT_TYPES_1, RESPONSE_GET_EVENT_TYPES_2]
 
-        r = self.calendly_client.get_all_event_types_of_user("user", "access_token", the_filter=ACTIVE_FILTER)
+        r = self.calendly_client.get_all_event_types_of_user("user", the_filter=ACTIVE_FILTER)
 
         expected = [
             {
