e21d7bce885135cd0f1c6e17d38da033b68bab41
Sebastian Warnke <sw-github@sebwarnke.com>
2021-06-06 00:06:44 +0200

    remove legacy configuration fields

--- src/helper.py
+++ src/helper.py
@@ -7,9 +7,6 @@ def reset_workflow_config(wf):
     try:
         wf.clear_settings()
         wf.clear_cache()
-        wf.delete_password(c.CLIENT_ID)
-        wf.delete_password(c.CLIENT_SECRET)
-        wf.delete_password(c.REFRESH_TOKEN)
         wf.delete_password(c.ACCESS_TOKEN)
     except:
         pass
