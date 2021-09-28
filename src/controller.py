bc7e7528456bd2c2ee58acbab01901a0ee93412d
Sebastian Warnke <sw-github@sebwarnke.com>
2021-09-28 22:00:46 +0200

    add controller and tests, yet incomplete

--- src/controller.py
+++ src/controller.py
@@ -1,9 +1,42 @@
 #!/usr/bin/python
 # encoding: utf-8
+from calendly_client import CalendlyClient, CalendlyClientException
+import constants as c
 
-def create_single_use_link(param):
-    return None
+
+class Controller:
+    calendly_client = None
+
+    def __init__(self, wf):
+        access_token = wf.get_password(c.ACCESS_TOKEN)
+        self.calendly_client = CalendlyClient(access_token)
+        self.stats = Stats(wf)
+
+    def create_single_use_link(self, event_type):
+        try:
+            link = self.calendly_client.create_link(event_type, 1)
+            self.stats.increment(event_type)
+
+        except CalendlyClientException:
+            pass
 
 
-def get_ordered_event_types():
-    return None
+    def get_ordered_event_types(self):
+        return None
+
+
+class Stats:
+
+    def __init__(self, wf):
+        event_stats = wf.settings.get(c.CONF_EVENT_STATS)
+        if event_stats is None:
+            wf.settings[c.CONF_EVENT_STATS] = {}
+
+        self.wf = wf
+
+    def increment(self, event_type):
+        if event_type in self.wf.settings[c.CONF_EVENT_STATS]:
+            current_count = self.wf.settings[c.CONF_EVENT_STATS][event_type]
+            self.wf.settings[c.CONF_EVENT_STATS][event_type] = current_count + 1
+        else:
+            self.wf.settings[c.CONF_EVENT_STATS][event_type] = 1
--- src/controller_test.py
+++ src/controller_test.py
@@ -2,55 +2,69 @@
 # encoding: utf-8
 
 import unittest
-
+from mock import patch, Mock
 import constants as c
-import controller
-from workflow import Workflow3
+from controller import Controller
 
 EVENT_TYPE = "event_type_foo"
 
 
 class ControllerTest(unittest.TestCase):
-    wf = None
 
     def setUp(self):
-        self.wf = Workflow3()
-        self.wf.clear_settings()
+        self.mock_wf = Mock()
+        self.mock_wf.get_password.return_value = "access_token"
 
-    def test_given_no_stats_when_create_single_use_link_then_count_is_one(self):
+    @patch("calendly_client.CalendlyClient.create_link")
+    def test_given_no_stats_when_create_single_use_link_then_count_is_one(self, mock_create_link):
         # Given, nothing
+        mock_create_link.return_value = "a_link"
+
+        self.mock_wf.settings = {}
+        controller = Controller(self.mock_wf)
 
         # When
         controller.create_single_use_link(EVENT_TYPE)
 
         # Then
-        event_count = self.wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
+        event_count = self.mock_wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
         self.assertEquals(event_count, 1)
 
-    def test_given_stats_exist_when_create_single_use_link_then_count_is_one(self):
+    @patch("calendly_client.CalendlyClient.create_link")
+    def test_given_stats_exist_when_create_single_use_link_then_count_is_one(self, mock_create_link):
         # Given
-        self.wf.settings[c.CONF_EVENT_STATS] = {"foo": 42}
+        mock_create_link.return_value = "a_link"
+        self.mock_wf.settings = {
+            c.CONF_EVENT_STATS: {"foo": 42}
+        }
+        controller = Controller(self.mock_wf)
 
         # When
         controller.create_single_use_link(EVENT_TYPE)
 
         # Then
-        event_count = self.wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
+        event_count = self.mock_wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
         self.assertEquals(event_count, 1)
 
-    def test_given_event_type_exists_in_stats_when_create_single_use_link_then_count_increments(self):
+    @patch("calendly_client.CalendlyClient.create_link")
+    def test_given_event_type_exists_in_stats_when_create_single_use_link_then_count_increments(self, mock_create_link):
         # Given
-        self.wf.settings[c.CONF_EVENT_STATS] = {EVENT_TYPE: 42}
+        mock_create_link.return_value = "a_link"
+        self.mock_wf.settings = {
+            c.CONF_EVENT_STATS: {EVENT_TYPE: 42}
+        }
+        controller = Controller(self.mock_wf)
 
         # When
         controller.create_single_use_link(EVENT_TYPE)
 
         # Then
-        event_count = self.wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
+        event_count = self.mock_wf.settings[c.CONF_EVENT_STATS][EVENT_TYPE]
         self.assertEquals(event_count, 43)
 
     def test_given_no_stats_when_get_event_types_then_empty_list_returns(self):
         # Given, nothing
+        controller = Controller(self.mock_wf)
 
         # When
         event_types = controller.get_ordered_event_types()
@@ -60,6 +74,7 @@ class ControllerTest(unittest.TestCase):
 
     def test_given_no_event_types_when_get_event_types_then_empty_list_returns(self):
         # Given, nothing
+        controller = Controller(self.mock_wf)
 
         # When
         event_types = controller.get_ordered_event_types()
@@ -67,11 +82,6 @@ class ControllerTest(unittest.TestCase):
         # Then
         self.assertDictEqual(event_types, {})
 
-    def test_given_no_stats_when_get_event_types_then_empty_list_returns(self):
-        # Given, nothing
 
-        # When
-        event_types = controller.get_ordered_event_types()
-
-        # Then
-        self.assertDictEqual(event_types, {})
+if __name__ == "__main__":
+    unittest.main()
