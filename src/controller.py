5d262c84c63fe445875054903eac70973e017a92
Sebastian Warnke <sw-github@sebwarnke.com>
2021-10-08 17:19:32 +0200

    add sorting capability of ranked event_types

--- src/controller.py
+++ src/controller.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # encoding: utf-8
-from calendly_client import CalendlyClient, CalendlyClientException
 import constants as c
+from calendly_client import CalendlyClient, CalendlyClientException
+from calendly_client import active_filter as ACTIVE_FILTER
 
 
 class Controller:
@@ -20,9 +21,27 @@ class Controller:
         except CalendlyClientException:
             pass
 
+    def get_ordered_event_types(self, user):
 
-    def get_ordered_event_types(self):
-        return None
+        ordered_event_types = []
+
+        unordered_event_types = self.calendly_client.get_all_event_types_of_user(user, the_filter=ACTIVE_FILTER)
+        if not unordered_event_types:
+            return []
+
+        event_stats = self.stats.get_stats()
+        if not event_stats:
+            return unordered_event_types
+
+        for event_stats_item in event_stats.items():
+            for i in range(len(unordered_event_types)):
+                needle = unordered_event_types[i]
+                if needle["uri"] == event_stats_item[0]:
+                    unordered_event_types[i]["event_stats"] = event_stats_item[1]
+
+        ordered_event_types = sorted(unordered_event_types, key=lambda event_type: event_type["event_stats"] if "event_stats" in event_type else None, reverse=True)
+
+        return ordered_event_types
 
 
 class Stats:
@@ -40,3 +59,8 @@ class Stats:
             self.wf.settings[c.CONF_EVENT_STATS][event_type] = current_count + 1
         else:
             self.wf.settings[c.CONF_EVENT_STATS][event_type] = 1
+
+    def get_stats(self):
+        return self.wf.settings[c.CONF_EVENT_STATS]
+
+
